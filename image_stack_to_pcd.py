5082dce9152ffc3bcb5b6aa75e9f54c961f91099
danekso <golubchikov.daniel@gmail.com>
2023-05-25 01:27:55 +0300

    docstrings rewritten

--- image_stack_to_pcd.py
+++ image_stack_to_pcd.py
@@ -10,18 +10,13 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 
 
-# comment
-
 def get_files_abs_paths (
         folder_path: str
         ):
-    """
-    From the certain folder create a string-format object (file.names), that includes a list of 'n' files in the
-    folder. Then, a subsequent extraction of the short path (key endings) of a file to the temp_list for further
-    processing or storage is accomplished.
-    :param: function receives a folder_path in a string format.
-    :return: function returns the temp_list in a string format. The intended purpose of the returned object is a file's
-    path in the folder in a cut format
+    """ Get the absolute file paths from the folder to the list (temp_list).
+
+    :param folder_path: a string contained folder path.
+    :return temp_list: a list contained strings with the absolute file paths.
     """
     print (f"Opened folder:\n{folder_path}")
     file_names = os.listdir (folder_path)
@@ -37,15 +32,13 @@ def get_files_abs_paths (
 def image_processing (
         image_path: str
         ):
-    '''
-    The function takes an image by the specified path (image.path). The received image is blurred (Gaussian blur)
-    with standard deviation set at 9 for both X and Y directions. Then, the image proceeds to the direct Otsu's
-    thresholding (cv.THRESH_BINARY+cv.THRESH_OTSU) with a subsequent extraction of non-zero values to the thrshInv
-    np.array object.
-    :param: function receives an image_path in a string format.
-    :return: function returns the NumPy object (np.array) threshInv. The intended purpose of the returned object is a
-    set of non-zero values (highlighted features) in the studied image.
-    '''
+    """ The image undergo processing, including Gaussian blurring and Otsu's thresholding. Then get all non-zero
+    values to the threshInv NumPy array.
+
+    :param image_path: a string contained the processed image's path.
+    :return threshInv: a NumPy object (np.array) contained a set of non-zero values (highlighted features) in the
+    studied image.
+    """
     # read img in gray format
     img = cv2.imread (image_path, 0)
 
@@ -67,22 +60,17 @@ def image_processing (
     return threshInv
 
 
+
 def create_layer (
         thresholding_result: np.array,
         layer_num: int
         ):
-    '''
-    The function creates a layer array by combining the thresholding results with the corresponding layer number.
-    Thereby, each element in the layer_arr corresponds to the given layer_num. Next, the function uses np.hstack to
-    horizontally stack the reshaped thresholding_result array (reshaped into a 2D array with two columns) with the
-    reshaped layer_arr array (reshaped into a 2D array with one column). This combines the thresholding results and
-    the layer numbers into a single array, where each row represents a data point with two thresholding values and a
-    corresponding layer number.
-    :param: function receives the thresholding_result NumPy array.
-    :param: function receives the layer_num integer.
-    :return: function returns the NumPy object (np.array) layer. The intended purpose of the returned object is a
-    set of reshaped layers for the further processing.
-    '''
+    """ Get numpy array with shape (n, 2). The numpy array with shape (n, 1) is created, filled with ones and added
+    to 1st one. The 3d column represents the layer number.
+    :param thresholding_result: a NumPy array contained the threshold result.
+    :param layer_num: an integer corresponded to the layer's number.
+    :return layer: a NumPy object (np.array) contained a set of reshaped layers for the further processing.
+    """
     layer_arr = np.ones (thresholding_result.shape [0]) * layer_num
 
     layer = np.hstack (
@@ -98,16 +86,12 @@ def create_layer (
 def process_files_in_folder (
         folder_path: str
         ):
-    '''
-    The function calls the get_files_abs_paths function to get the absolute paths for the files in the folder. Then,
-    for each file in the directory it performs image processing by image_processing function (stored in temp_res),
-    creates a layer array from the thresholding results (stored in temp_res), removes the file extension from the
-    file name, and step-by-step adds the temp_res layer array to the stack_layers dictionary using the extracted file
-    name as the key.
-    :param: function receives the folder_path string.
-    :return: function returns the NumPy object (dictionary) stack_layers. The intended purpose of the returned
-    object is the set of layer arrays ready for the combining.
-    '''
+    """ Apply the get_files_abs_paths function to receive abs paths. Then make image processing and reshape the
+    obtained arrays, and put it to the temp_res array. Match it in the dictionary with the split file's path.
+
+    :param folder_path: a string contained folder path.
+    :return stack_layers: a dictionary with the set of layer arrays.
+    """
     files_path_list = get_files_abs_paths (folder_path = folder_path)
 
     print (f"IMAGE PROCESSING STAGE\n")
@@ -133,14 +117,12 @@ def process_files_in_folder (
 def tissue_reconstruction (
         stack: dict
         ):
-    '''
-    The function firstly creates a 3-dimensional array filled with ones. Subsequently, the function
-    performs the vertically stacking of the processed layer array on the top of the temp_df NumPy array. This step is
-    then performed for each layer kept in the stack_layers dictionary.
-    :param: function receives the stack NumPy dictionary.
-    :return: function returns the NumPy object (3-dimensional array) temp_df. The intended purpose of the returned
-    object is the combined layer for the subsequent processing.
-    '''
+    """ A 3d numpy array filled with ones was created. Then step-by-step it is filled from the top with layers taken
+    from the stack_layers dictionary.
+
+    :param dictionary: a dictionary contained the set of layer arrays.
+    :return temp_df: a NumPy object (3-dimensional array) contained the combined layer.
+    """
     temp_df = np.ones (3)
     print (f'The df size is: {temp_df.shape}')
 
